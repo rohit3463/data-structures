4790043fa4280984e038527c14edf83c7c5fd2d1
rohit3463 <rohit.sethi9139666@gmail.com>
2018-06-12 11:22:39 +0530

    binary tree level order insertion

--- levelorder.cpp
+++ levelorder.cpp
@@ -30,6 +30,33 @@ public:
 			temp.pop();
 		}
 	}
+	void insert_node(int taken)
+	{
+		queue < mytree * > temp;
+		mytree * it = this ;
+
+		while(it != nullptr)
+		{
+			if(it->left_node == nullptr)
+			{
+				it->left_node = new mytree(taken);
+				return;
+			}
+			else if (it->right_node == nullptr)
+			{
+				it->right_node = new mytree(taken);
+				return;
+			}
+			else
+			{
+				temp.push(it->left_node);
+				temp.push(it->right_node);
+
+				it = temp.front();
+				temp.pop();
+			}
+		}
+	}
 	mytree *& left()
 	{
 		return this->left_node;
@@ -49,9 +76,12 @@ int main()
 {
 	mytree * obj = new mytree(1);
 
-	obj->left() = new mytree(2);
-
-	obj->right() = new mytree(3);
+	obj->insert_node(2);
+	obj->insert_node(3);
+	obj->insert_node(4);
+	obj->insert_node(5);
+	obj->insert_node(6);
+	obj->insert_node(7);
 
 	obj->levelordertraverse();
 
