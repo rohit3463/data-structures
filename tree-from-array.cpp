2b8a713f820fbe8e239eff15e09ae87fbd998ef3
rohit3463 <rohit.sethi9139666@gmail.com>
2018-06-28 17:25:55 +0530

    binary tree from an array representation of a binary tree

---
+++ tree-from-array.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+
+using namespace std;
+
+struct mytree
+{
+	int data;
+	mytree * left = nullptr;
+	mytree * right = nullptr;
+};
+void tree_from_array(int arr[],int index,mytree *& node,int size)
+{
+	if(index < size)
+	{
+		node = new mytree;
+
+		node->data = arr[index];
+
+		tree_from_array(arr,2*index + 1,node->left,size);
+
+		tree_from_array(arr,2*index + 2,node->right,size);
+
+	}
+	return;
+}
+void inorder_traverse(mytree * root)
+	{
+		if(root == nullptr)
+			return;
+
+		inorder_traverse(root->left);
+
+		cout<<root->data<<" ";
+
+		inorder_traverse(root->right);
+
+		return;
+	}
+
+int main()
+{
+	mytree * root = nullptr;
+	int size;
+
+	int arr[6] = {10,12,15,25,30,36};
+
+	size = sizeof(arr)/sizeof(int);
+
+	tree_from_array(arr,0,root,size);
+
+	inorder_traverse(root);
+
+	return 0;
+}
