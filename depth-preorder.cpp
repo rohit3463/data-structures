67a4d2f404c389bf7afba72f448f3c5a495fa09b
rohit3463 <rohit.sethi9139666@gmail.com>
2018-07-02 21:45:38 +0530

    depth of a binary tree thorugh preorder traversal

---
+++ depth-preorder.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int depth_preorder(string& str)
+{
+	static int index = 0;
+
+	if(index >= str.size() || str[index] == 'l')
+	{
+		return 0;
+	}
+
+	index++;
+
+	int left = depth_preorder(str);
+
+	index++;
+
+	int right = depth_preorder(str);
+
+	return max(left,right) + 1;
+}
+
+int main()
+{
+	string str = "nlnnlll";
+
+	int depth = depth_preorder(str);
+
+	cout<<depth<<endl;
+}
