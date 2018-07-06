5189be7f4e7b0f4f0edc9034988b76114e4cd0c0
rohit3463 <rohit.sethi9139666@gmail.com>
2018-07-06 15:04:53 +0530

    check for each node to be sum of it's children in a binary tree

---
+++ binary tree/each-node-is-sumofchildren.cpp
@@ -0,0 +1,61 @@
+/* 
+Source:-https://www.geeksforgeeks.org/check-for-children-sum-property-in-a-binary-tree/
+*/
+#include<iostream>
+
+using namespace std;
+
+struct node{
+  int data;
+  node *left, *right;
+};
+
+struct node* newNode(int data){
+  struct node *Node = new node;
+  Node->data = data;
+  Node->left= nullptr;
+  Node->right = nullptr;
+
+  return Node;
+}
+
+bool sum_property(node *root){
+
+    int new_data;
+
+    if(root == nullptr)
+        return true;
+
+    if(root->left != nullptr && root->right == nullptr)
+        new_data = root->left->data;
+
+    else if(root->left == nullptr && root->right != nullptr)
+        new_data = root->right->data;
+
+    else if(root->left != nullptr && root->right != nullptr)
+        new_data = root->left->data + root->right->data;
+
+    else return true;
+
+    if(root->data == new_data){
+        return true && sum_property(root->left) && sum_property(root->right);
+    }
+
+    else return false;
+}
+
+int main(){
+
+    struct node *root  = newNode(10);
+      root->left         = newNode(8);
+      root->right        = newNode(2);
+      root->left->left   = newNode(3);
+      root->left->right  = newNode(5);
+      root->right->right = newNode(2);
+  
+      if(sum_property(root))
+          cout<<"The given tree satisfies the children sum property"<<endl;
+
+      else cout<<"The given tree doesn't satisfy the children sum property"<<endl;
+    return 0;
+}
