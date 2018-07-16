1cc2f53ee036b4ea29906b19f1b22f7ca74b9cf5
rohit3463 <rohit.sethi9139666@gmail.com>
2018-07-17 01:31:35 +0530

    population outburst hackerearth

---
+++ binary tree/population-outburst.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<queue>
+#include<unordered_map>
+#include<utility>
+
+using namespace std;
+ 
+int main()
+{
+    long long N,kid,parent=0,j=1;
+    cin>>N;
+    cin>>kid;
+    
+    queue<pair<long long,long long>>hello;
+    unordered_map < long long,long long> level;
+    
+    while(kid--)
+    {
+        hello.push(make_pair(parent,j));
+        j++;
+    }
+    level[0] = 0;
+    
+    for(long long i=0;i<N;i++)
+    {
+        j=1;
+        cin>>parent;
+        cin>>kid;
+        level[parent] = level[hello.front().first];
+        cout<<hello.front().first<<level[parent]<<hello.front().second<<endl;
+
+        hello.pop();
+        
+        while(kid--)
+        {
+            hello.push(make_pair(parent,j));
+            j++;
+        }
+    }
+    
+    return 0;
+}
