4e3796034c8a7cdeadfdee198c42d4a1b5494618
rohit3463 <rohit.sethi9139666@gmail.com>
2018-07-17 12:56:30 +0530

    searching a node in binary search tree

---
+++ binary-search-tree/search-a-node.cpp
@@ -0,0 +1,98 @@
+#include<iostream>
+
+using namespace std;
+
+struct node
+{
+	int data;
+	node * left = nullptr;
+	node * right = nullptr;
+};
+
+struct node * newNode(int key)
+{
+	node * temp = new node;
+	temp->data = key;
+	temp->left = nullptr;
+	temp->right = nullptr;
+
+	return temp;
+}
+
+node * insert(node * root,int key)
+{
+	if(root == nullptr)
+	{
+		return newNode(key);
+	}
+
+	if(key < root->data)
+	{
+		root->left = insert(root->left,key);
+	}
+	else if(key > root->data)
+	{
+		root->right = insert(root->right,key);
+	}
+
+	return root;
+}
+
+node * search(node * root,int key)
+{
+	if(root == nullptr || root->data == key)
+	{
+		return root;
+	}
+
+	if(key>root->data)
+	{
+		return search(root->right,key);
+	}
+
+	return search(root->left,key);
+}
+
+void preorder(node * root)
+{
+	if(root == nullptr)
+	{
+		return;
+	}
+	cout<<root->data<<" ";
+
+	preorder(root->left);
+	preorder(root->right);
+}
+
+int main()
+{
+	node * root = nullptr;
+
+	root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 70);
+    insert(root, 60);
+    insert(root, 80);
+
+    preorder(root);
+
+    cout<<endl;
+
+    if(search(root,70) != nullptr)
+    {
+    	cout<<"YES";
+    }
+    else
+    {
+    	cout<<"NO";
+    }
+
+    cout<<endl;
+
+
+
+	return 0;
+}
