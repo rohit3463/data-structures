765811968c747b1d2c96d5c06cfbeb2868b29ff3
rohit3463 <rohit.sethi9139666@gmail.com>
2018-10-01 21:05:08 +0530

    updated:- knapsack 1/0

--- algorithm/knapsack.cpp
+++ algorithm/knapsack.cpp
@@ -5,16 +5,28 @@ using namespace std;
 
 int knapsack(int n,int W,int w[],int val[])
 {
-	if(w[n-1] == 0 || W == 0)
+	int k[n+1][W+1];
+
+	for(int i=0;i<=n;i++)
 	{
-		return 0;
-	}
-	if(w[n-1] > W)
-	{
-		knapsack(n-1,W,w,val);
+		for(int j = 0;j<=W;j++)
+		{
+			if(i==0 || j==0)
+			{
+				k[i][j] = 0;
+			}
+			else if(w[i-1] <= j)
+			{
+				k[i][j] = max(val[i-1] + k[i-1][j- w[i-1]],k[i-1][j]);
+			}
+			else
+			{
+				k[i][j] = k[i-1][j];
+			}
+		}
 	}
 
-	return max(val[n-1] + knapsack(n-1,W - w[n-1],w,val),knapsack(n-1,W,w,val));
+	return k[n][W];
 }
 
 int main()
